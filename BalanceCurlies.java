c34919485675c0dada62c79bb1be38f37560af2e
aperks-17 <46737512+aperks-17@users.noreply.github.com>
2019-01-28 22:03:26 -0700

    Update BalanceCurlies.java

--- BalanceCurlies.java
+++ BalanceCurlies.java
@@ -12,17 +12,19 @@ public class BalanceCurlies {
         System.out.println("Please enter curlie brackets");
         String x=input.next();
         int count=0;
-       for(int i=0; i<=x.length;i++);{
-           if(x(i)=='{')
-           counter++;
-           if(x(i)=='}')
-           counter--;
-       }
-    if(counter==0)
+        int length=x.length();
+       for(int i=0; i<length;i++){  
+        if(x.charAt(i)=='{')
+           count++;
+           if(x.charAt(i)=='}')
+           count--;
+
+        }
+    if(count==0)
     System.out.println("The curlies are balanced!");
-    else if(counter<0)
+    else if(count<0)
     System.out.println("There are too many closed brackets");
-    else if(counter>0)
+    else if(count>0)
     System.out.println("There are too many opened brackets");
     }
 
